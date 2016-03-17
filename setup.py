86d62765f1ef24efa8fd8ca9fe89a22b35b366b8
Christian Stewart <christian@paral.in>
2016-03-17 16:24:02 +0000

    v1.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['click', 'pykube', 'pyyaml']
 
 setup(
     name='openshift-under-kubernetes',
-    version='1.2.0',
+    version='1.2.1',
     url='https://github.com/paralin/openshift-under-kubernetes',
     license='BSD',
     author='Christian Stewart',
